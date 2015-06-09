04d6e5868b4b7700bd1c332ac8911afc7b661bb7
Paul Woolcock <paul@woolcock.us>
2015-06-09 07:32:17 -0400

    Add mod_exp function and a test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
-#[test]
-fn it_works() {
+pub fn mod_exp(base: i64, exponent: i64, modules: i64) -> i64 {
+    0i64
+}
+
+#[cfg(test)] mod tests {
+    use super::mod_exp;
+
+    #[test]
+    fn test_mod_exp() {
+        let base = 4i64;
+        let exponent = 13i64;
+        let modulus = 497i64;
+        assert_eq!(mod_exp(base, exponent, modulus), 445i64);
+    }
 }
