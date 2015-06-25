05aecdcb589cbe3433b727fa507747c76f974920
Paul Woolcock <paul@woolcock.us>
2015-06-25 10:04:15 -0400

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,39 @@
+//! Modular Exponentiation
+//!
+//! Implementation of a modular exponentiation algorithm based on the algorithm from
+//! 'Applied Cryptography'. You can find more details in that book, or on
+//! [wikipedia](https://en.wikipedia.org/wiki/Modular_exponentiation#Right-to-left_binary_method).
+
 extern crate num;
 
 use std::ops::{Shr};
 use num::traits::{Num, One, Zero, Bounded};
 
 #[allow(non_snake_case)]
+/// Performs the exponentiation
+///
+/// All parameters are generic, provided they implement the following traits:
+///
+/// * Num
+/// * PartialOrd
+/// * Shr<T, Output=T>
+/// * Copy
+/// * Bounded
+///
+/// You can find the `Num` and `Bounded` traits in the [num](https://crates.io/crate/num) crate.
+///
+/// # Examples
+///
+/// ```
+/// use mod_exp::mod_exp;
+///
+/// assert_eq!(mod_exp(5, 3, 13), 8);
+/// ```
+///
+/// # Panics
+///
+/// The function does an `assert!` to verify that the data type of `base` is
+/// large enough that the result won't overflow during the computation
 pub fn mod_exp<T>(base: T, exponent: T, modulus: T) -> T where T: Num + PartialOrd + Shr<T, Output=T> + Copy + Bounded {
     let ONE: T = One::one();
     let TWO: T = ONE + ONE;
